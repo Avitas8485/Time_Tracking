c19e19715ebdc913c0e1883e9b50f9605931f65a
Avity <irritas8485@gmail.com>
2023-11-09 15:45:17 -0500

    Add database functionality to track active window time

--- active_window.py
+++ active_window.py
@@ -2,13 +2,13 @@ import time
 import win32gui
 import psutil
 import win32process
-
+import sqlite3
 
 
 
 
 class ActiveWindowTracker:
-    def __init__(self):
+    def __init__(self, db_name="window_tracker.db"):
         """Initializes the ActiveWindowTracker class
         Attributes:
             current_window (tuple): (title, exe, pid, path) of the currently active window
@@ -17,6 +17,26 @@ class ActiveWindowTracker:
         self.current_window = None
         self.start_time = time.time()
         self.active_time = 0
+
+        self.conn = sqlite3.connect(db_name)
+        
+
+    def create_table(self):
+        """Creates a table in the database to store the active window information"""
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS window_activity (
+                       title TEXT,
+                       exe TEXT,
+                       pid INTEGER,
+                       path TEXT,
+                       start_time REAL,
+                       active_time REAL
+                       )
+                       """)
+        self.conn.commit()
+
+                       
 
     def get_active_window_info(self):
         """Returns the title, exe, pid, and path of the currently active window"""
@@ -29,7 +49,7 @@ class ActiveWindowTracker:
     def track_active_window_time(self):
         """Returns the title and active time of the currently active window
         Returns:
-            tuple: (title, active_time)"""
+            tuple: (title, exe, pid, path, active_time)"""
         active_window = self.get_active_window_info()
         if active_window != self.current_window:
             if self.current_window is not None:
@@ -38,10 +58,30 @@ class ActiveWindowTracker:
             self.current_window = active_window
             self.start_time = time.time()
         time.sleep(0.1)
-        return self.current_window[0], self.active_time
+        return self.current_window, self.active_time
 
+    def store_window_activity(self, title, exe, pip, path, active_time):
+        """Store window activity in the database"""
+        cursor = self.conn.cursor()
+        cursor.execute("""
+            INSERT INTO window_activity (title, exe, pid, path, start_time, active_time)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """, (title, exe, pip, path, self.start_time, active_time))
+        self.conn.commit()
+
+    def close_database_connection(self):
+        """Closes the database connection"""
+        self.conn.close()
 
 if __name__ == "__main__":
     tracker = ActiveWindowTracker()
-    while True:
-        tracker.track_active_window_time()
+    try:
+        while True:
+            active_window, active_time = tracker.track_active_window_time()
+            tracker.create_table()
+            tracker.store_window_activity(*active_window, active_time)
+    except KeyboardInterrupt:
+        tracker.close_database_connection()
+        print("Database connection closed")
+        print("Program terminated")
+
